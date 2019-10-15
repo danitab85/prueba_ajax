9ef5d49b7c48f9a030631bf59a8ac443b4433da7
danitab85 <danielaballesteros85@gmail.com>
2019-10-15 15:26:40 -0300

    Punto 1 y 2: Creación panel admin(active admin) Crear, editar y eliminar

---
+++ app/admin/users.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :password
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  form do |f|
+    input :email
+    input :password
+    actions
+  end
+
+  controller do
+    def update
+      if (params[:user][:password].blank? && params[:user][:password_confirmation].blank?)
+        params[:user].delete("password")
+        params[:user].delete("password_confirmation")
+      end
+      super
+    end
+  end
+end
