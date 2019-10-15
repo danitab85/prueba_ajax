ef0acb917a7949fcc8869d11bef850038369b552
danitab85 <danielaballesteros85@gmail.com>
2019-10-15 11:21:27 -0300

    Borrar y editar empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -44,7 +44,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to companies_path, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
@@ -60,6 +60,7 @@ class CompaniesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to companies_url, notice: 'Company was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
