00afcce296252f9daa03d9819919e72906f6c86e
danitab85 <danielaballesteros85@gmail.com>
2019-10-15 12:50:46 -0300

    navbar

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   before_action :set_company, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user, only: [:show]
+  before_action :authenticate_user!, only: [:show]
 
   # GET /companies
   # GET /companies.json
